08c1ba83e9f3a4abc1a916c1441a90d737f8ce9c
unknown <imdadulhoquekk@gmail.com>
2026-02-04 21:07:03 +0530

    Setting up express Routes

--- 04_Rest-Api/index.js
+++ 04_Rest-Api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const userRoutes = require('./routes/users.routes.js')
 
 const PORT = 8000;
 
 const app = express();
 
 app.use( express.json());
+
+app.use("/api/users" , userRoutes);
 
 app.listen(PORT , ()=>{
     console.log(`Server is listening on port : ${PORT}`);
---
+++ 04_Rest-Api/routes/users.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { getUsers ,getUserById ,createUser ,updateUser,deleteUser } = require("../controller/users.controller.js");
+
+router.get("/" , getUsers);
+ router.get("/:id" , getUserById);
+ router.post("/" , createUser)
+ router.put("/:id" , updateUser);
+ router.delete("/:id" , deleteUser);
+
+
+module.exports = router; 
