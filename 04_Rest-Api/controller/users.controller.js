2504f88e399c8ccc0e6c2802366da50746324ec2
unknown <imdadulhoquekk@gmail.com>
2026-02-06 23:44:41 +0530

    add delete logic

--- 04_Rest-Api/controller/users.controller.js
+++ 04_Rest-Api/controller/users.controller.js
@@ -55,6 +55,40 @@ const createUser = async (req , res)=>{
     }
 }
 
+const updateUser =async (req  , res)=>{
+try {
+    
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+          res.status(404).json({ msg: "User not Found!"});
+    }
+
+
+} catch (error) {
+    
+}
+
+}
+
+const deleteUser = async (req, res)=>{
+
+try {
+    
+    const id = Number(req.params.id);
+    const index =await User.findIndex(user => user.id === id);
+if(index = -1 ){
+    res.status(404).end("Nothing to deleted");
+}
+    User.splice(index , 1);
+    res.status(400).json({ msg: "delete Succesfully"});
+
+} catch (error) {
+            res.status(500).json({msg : " internal server  Error" , Error: error.msg}) 
+
+}
+
+}
 
 module.exports = {
      getUsers ,
