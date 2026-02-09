56b8ab02afde4d7eff10e95c346908131b14e6e8
unknown <imdadulhoquekk@gmail.com>
2026-02-09 20:54:23 +0530

    Add user Schema

---
+++ 04_Rest-Api/models/user.models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+           type : String ,
+        required : true ,
+    } ,
+     email: {
+           type : String ,
+        required : true , 
+        unique : true ,
+     } ,
+     gender : {
+        type : String ,
+     } ,
+    
+     avatar : {
+        type : String ,
+        default :  "https://robohash.org/voluptatempariaturducimus.png?size=50x50&set=set1" ,
+     }
+
+} , {
+    timestamps : true 
+})
+
+
+const User = mongoose.model("user" ,userSchema);
+
+module.exports = User ; 
