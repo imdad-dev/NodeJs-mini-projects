88e132a9b6274cea9fb22ea593352e87f2264775
unknown <imdadulhoquekk@gmail.com>
2026-02-09 21:47:41 +0530

    verify all Routes using postman except updateUser

--- 04_Rest-Api/index.js
+++ 04_Rest-Api/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const userRoutes = require('./routes/users.routes.js')
+const connectDb = require('./DB/connect.js')
+
 
 const PORT = 8000;
 
 const app = express();
 
 app.use( express.json());
+app.use(express.urlencoded({ extended : false }))
 
-app.use("/api/users" , userRoutes);
+// connect Mongoose 
+connectDb('mongodb://localhost:27017/Rest-Api')
+
+app.use("/api/user" , userRoutes);
 
 app.listen(PORT , ()=>{
     console.log(`Server is listening on port : ${PORT}`);
