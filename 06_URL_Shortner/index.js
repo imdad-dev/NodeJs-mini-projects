ad496666acfe330fffcb48ce8d0b239b0185e81d
unknown <imdadulhoquekk@gmail.com>
2026-02-10 21:17:45 +0530

    feat: added url Schema and connect Mongo

--- 06_URL_Shortner/index.js
+++ 06_URL_Shortner/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const connectDB = require("./DB/connectDB.js")
 
 const app = express();
 const PORT = 3000;
 
+
+// mongo Connect 
+connectDB("mongodb://localhost:27017/url-shortner") ;
 
 // middleware 
 app.use(express.json());
