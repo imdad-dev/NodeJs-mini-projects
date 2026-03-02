ea94bb7eadec9e30a64f3ff58e22ec516f3dd114
unknown <imdadulhoquekk@gmail.com>
2026-03-02 15:35:45 +0530

    Refactor & feat (skills) : Seperate app.js from index.js for modularity and add skills page with bootstrap progress bar

---
+++ 07_Personal-Portfolio/app.js
@@ -0,0 +1,52 @@
+import express from "express"
+
+
+import Project from "./models/project.model.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs'); // Set EJS as template engine
+app.use(express.static('public')); // Serve static files from public folder
+
+app.get('/', (req, res) => { 
+  res.render("home")
+});
+
+app.get('/about', (req, res) => { 
+  res.render("about")
+});
+
+
+app.get("/test-db" , async (req, res)=>{
+
+  const project =  await Project.create(  {
+    title : "Test title project" ,
+   
+    description : "This is test portfolio project" ,
+    link : "http://imdad-dev/portfolio"
+
+  });
+  console.log("prject: " , project);
+
+  res.send(" created project model on mongoDB")
+
+})   
+
+app.get("/project" , async (req , res)=>{
+
+  const projects = await Project.find ();  // fetch all projectc
+  console.log(projects)
+  res.render("project" , { 
+    projects 
+  })
+
+})
+
+
+app.get("/skills" , (req , res)=>{
+    res.render("skills")
+})
+
+
+export default app;
--- 07_Personal-Portfolio/index.js
+++ 07_Personal-Portfolio/index.js
@@ -1,53 +1,15 @@
 import dotenv from  "dotenv"
-import express from "express"
+import app from "./app.js"
 import connectMongoDB from "./DB/connectDB.js";
-import Project from "./models/project.model.js";
 
 dotenv.config();
 
 
-const app = express();
+ 
 const port = process.env.PORT || 3000;
 
 // mongo connect
 connectMongoDB(process.env.MONGODB_URI);
-
-app.set('view engine', 'ejs'); // Set EJS as template engine
-app.use(express.static('public')); // Serve static files from public folder
-
-app.get('/', (req, res) => { 
-  res.render("home")
-});
-
-app.get('/about', (req, res) => { 
-  res.render("about")
-});
-
-
-app.get("/test-db" , async (req, res)=>{
-
-  const project =  await Project.create(  {
-    title : "Test title project" ,
-   
-    description : "This is test portfolio project" ,
-    link : "http://imdad-dev/portfolio"
-
-  });
-  console.log("prject: " , project);
-
-  res.send(" created project model on mongoDB")
-
-})   
-
-app.get("/project" , async (req , res)=>{
-
-  const projects = await Project.find ();  // fetch all projectc
-  console.log(projects)
-  res.render("project" , { 
-    projects 
-  })
-
-})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
